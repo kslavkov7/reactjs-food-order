35103a4f01b05ca7d38c1d05319946e58a1ba405
Kiril Slavkov <kiko19141914@gmail.com>
2025-01-11 22:35:33 +0200

    added cart modal

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import { useState } from 'react';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
+import Cart from './components/Cart/Cart'
+
 
 function App() {
   useState();
 
   return (
     <CartProvider>
+      <Cart />
       <Header />
       <main>
         <Meals />
